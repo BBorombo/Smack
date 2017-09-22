f4896ab193e4f08219e7f7721484f65844670de1
Borombo <e.rombo@yahoo.com>
2017-09-22 15:20:47 +0200

    Web request init

---
+++ Smack/Services/AuthService.swift
@@ -0,0 +1,72 @@
+//
+//  AuthService.swift
+//  Smack
+//
+//  Created by Erwan Rombo on 22/09/2017.
+//  Copyright © 2017 Borombo. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class AuthService {
+    
+    static let instance = AuthService()
+    
+    let defaults = UserDefaults.standard
+    
+    // Boolean - isLogged
+    var isLoggedIn: Bool{
+        get{
+            return defaults.bool(forKey: LOGGED_IN_KEY)
+        }
+        set{
+            defaults.set(newValue, forKey: LOGGED_IN_KEY)
+        }
+    }
+    
+    // Token
+    var authToken: String {
+        get{
+            return defaults.value(forKey: TOKEN_KEY) as! String
+        }
+        set{
+            defaults.set(newValue, forKey: TOKEN_KEY)
+        }
+    }
+    
+    // User Email
+    var userEmail: String {
+        get{
+            return defaults.value(forKey: USER_EMAIL) as! String
+        }
+        set{
+            defaults.set(newValue, forKey: USER_EMAIL)
+        }
+    }
+    
+    func registerUser(email: String, password: String, completion: @escaping CompletionHandler){
+        
+        let lowercaseEmail = email.lowercased()
+        
+        let header = [
+            "Content-Type": "application/json; charset=utf-8"
+        ]
+        
+        let body: [String: Any] = [
+            "email": lowercaseEmail,
+            "password": password
+        ]
+        
+        Alamofire.request(URL_REGISTER, method: .post, parameters: body, encoding: JSONEncoding.default, headers: header).responseJSON { (response) in
+            if response.result.error == nil{
+                completion(true)
+            }else{
+                completion(false)
+                debugPrint(response.result.error as Any)
+            }
+        }
+        
+    }
+    
+}
