8a6564b57021b9081d2386c80faec3d3ad656ea0
Borombo <e.rombo@yahoo.com>
2017-09-22 11:37:04 +0200

    SWRevealView Setup

---
+++ Smack/Controller/ChannelVC.swift
@@ -0,0 +1,22 @@
+//
+//  ChannelVC.swift
+//  Smack
+//
+//  Created by Erwan Rombo on 22/09/2017.
+//  Copyright © 2017 Borombo. All rights reserved.
+//
+
+import UIKit
+
+class ChannelVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 60
+    
+    }
+
+    
+    
+
+}
---
+++ Smack/Controller/ChatVC.swift
@@ -0,0 +1,34 @@
+//
+//  ChatVC.swift
+//  Smack
+//
+//  Created by Erwan Rombo on 22/09/2017.
+//  Copyright © 2017 Borombo. All rights reserved.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+
+    // Outlets
+    @IBOutlet weak var menuButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        menuButton.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+        
+        self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+ 
+
+}
