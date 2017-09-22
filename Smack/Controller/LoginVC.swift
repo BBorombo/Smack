8d3a55af72c827438a405f6d76d86cc800158947
Borombo <e.rombo@yahoo.com>
2017-09-22 12:34:38 +0200

    Login VC UI

---
+++ Smack/Controller/LoginVC.swift
@@ -0,0 +1,24 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by Erwan Rombo on 22/09/2017.
+//  Copyright © 2017 Borombo. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+    
+    //Outlets
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+     
+    }
+
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
